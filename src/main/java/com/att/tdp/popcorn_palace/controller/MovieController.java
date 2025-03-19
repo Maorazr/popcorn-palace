d083e52000600d5afcddfb39decfd37e2b83c960
Maorazr <maoraz@post.bgu.ac.il>
2025-03-19 14:00:09 +0200

    add movie service, modified movie controller to work with service

--- src/main/java/com/att/tdp/popcorn_palace/controller/MovieController.java
+++ src/main/java/com/att/tdp/popcorn_palace/controller/MovieController.java
@@ -3,80 +3,46 @@ package com.att.tdp.popcorn_palace.controller;
 
 import com.att.tdp.popcorn_palace.dto.MovieDto;
 import com.att.tdp.popcorn_palace.entity.Movie;
-import com.att.tdp.popcorn_palace.repository.MovieRepository;
+import com.att.tdp.popcorn_palace.service.MovieService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/movies")
 @RequiredArgsConstructor
 public class MovieController {
 
-    private final MovieRepository movieRepository;
-
-//    public MovieController(MovieRepository movieRepository) {
-//        this.movieRepository = movieRepository;
-//    }
+    private final MovieService movieService;
 
     // GET /movies/all
     @GetMapping("/all")
     public ResponseEntity<List<MovieDto>> getAllMovies() {
-        List<MovieDto> movies = movieRepository.findAll().stream()
-                .map(this::toDto)
-                .toList();
-
+        List<MovieDto> movies = movieService.getAllMovies();
         return ResponseEntity.ok(movies);
     }
 
     // POST /movies
     @PostMapping
     public ResponseEntity<MovieDto> addMovie(@Valid @RequestBody MovieDto movieDto) {
-        Movie movie = Movie.builder()
-                .title(movieDto.getTitle())
-                .genre(movieDto.getGenre())
-                .duration(movieDto.getDuration())
-                .releaseYear(movieDto.getReleaseYear())
-                .build();
-
-        Movie savedMovie = movieRepository.save(movie);
-        return ResponseEntity.ok(toDto(savedMovie));
+        MovieDto created = movieService.createMovie(movieDto);
+        return ResponseEntity.ok(created);
     }
 
     // POST /movies/update/{movieTitle}
     @PostMapping("/update/{movieTitle}")
     public ResponseEntity<Void> updateMovie(@PathVariable String movieTitle,@Valid @RequestBody MovieDto updatedMovieDto) {
-        Optional<Movie> optionalMovie = movieRepository.findByTitle(movieTitle);
-        if (optionalMovie.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        Movie exsitingMovie = optionalMovie.get();
-
-        exsitingMovie.setGenre((updatedMovieDto.getGenre()));
-        exsitingMovie.setDuration(updatedMovieDto.getDuration());
-        exsitingMovie.setRating(updatedMovieDto.getRating());
-        exsitingMovie.setReleaseYear(updatedMovieDto.getReleaseYear());
-
-        movieRepository.save(exsitingMovie);
+        movieService.updateMovie(movieTitle, updatedMovieDto);
         return ResponseEntity.ok().build();
     }
 
     // DELETE /movie/{movieTitle}
     @DeleteMapping("/{movieTitle}")
     public ResponseEntity<Void> deleteMovie(@PathVariable String movieTitle) {
-        Optional<Movie> optionalMovie = movieRepository.findByTitle(movieTitle);
-        if (optionalMovie.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        movieRepository.delete(optionalMovie.get());
+        movieService.deleteMovie(movieTitle);
         return ResponseEntity.ok().build();
     }
 
---
+++ src/main/java/com/att/tdp/popcorn_palace/service/MovieService.java
@@ -0,0 +1,73 @@
+package com.att.tdp.popcorn_palace.service;
+
+import com.att.tdp.popcorn_palace.dto.MovieDto;
+import com.att.tdp.popcorn_palace.entity.Movie;
+import com.att.tdp.popcorn_palace.repository.MovieRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+
+    public List<MovieDto> getAllMovies() {
+        return movieRepository.findAll()
+                .stream()
+                .map(this::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+
+    public MovieDto createMovie(MovieDto movieDto) {
+        Movie movie = dtoToEntity(movieDto);
+        Movie saved = movieRepository.save(movie);
+        return entityToDto(saved);
+    }
+
+    public void updateMovie(String title, MovieDto movieDto) {
+        Movie existing = movieRepository.findByTitle(title)
+                .orElseThrow(() -> new RuntimeException("Movie not found"));
+
+        existing.setGenre(movieDto.getGenre());
+        existing.setDuration(movieDto.getDuration());
+        existing.setRating(movieDto.getRating());
+        existing.setReleaseYear(movieDto.getReleaseYear());
+
+        Movie updated = movieRepository.save(existing);
+    }
+
+    public void deleteMovie(String title) {
+        Movie existing = movieRepository.findByTitle(title)
+                .orElseThrow(() -> new RuntimeException("Movie not found"));
+        movieRepository.delete(existing);
+    }
+
+    private Movie dtoToEntity(MovieDto dto) {
+        return Movie.builder()
+                .title(dto.getTitle())
+                .genre(dto.getGenre())
+                .duration(dto.getDuration())
+                .rating(dto.getRating())
+                .releaseYear(dto.getReleaseYear())
+                .build();
+    }
+
+    private MovieDto entityToDto(Movie movie) {
+        return MovieDto.builder()
+                .id(movie.getId())
+                .title(movie.getTitle())
+                .genre(movie.getGenre())
+                .duration(movie.getDuration())
+                .rating(movie.getRating())
+                .releaseYear(movie.getReleaseYear())
+                .build();
+    }
+}
