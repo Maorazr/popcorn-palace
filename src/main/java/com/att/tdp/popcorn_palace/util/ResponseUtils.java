ff44d0b05f28a3951c544eba2d2f50aa441a3b15
Maorazr <maoraz@post.bgu.ac.il>
2025-03-21 13:45:30 +0200

    add Response utility class

---
+++ src/main/java/com/att/tdp/popcorn_palace/util/ResponseUtils.java
@@ -0,0 +1,14 @@
+package com.att.tdp.popcorn_palace.util;
+
+import org.springframework.http.ResponseEntity;
+
+public class ResponseUtils {
+
+  private ResponseUtils() {}
+
+  public static ResponseEntity<Void> ok() {
+    return ResponseEntity.ok().build();
+  }
+
+
+}
